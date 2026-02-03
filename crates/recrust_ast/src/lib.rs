7bdfaaf4037e452c67e6a7763404f7aefdf6d686
Covei Rares <rarescovei5@gmail.com>
2026-02-03 21:13:09 +0200

    Removed unused code

--- crates/recrust_ast/src/lib.rs
+++ crates/recrust_ast/src/lib.rs
@@ -1,13 +1,11 @@
 pub mod component;
 pub mod element;
-pub mod location;
 pub mod node;
 pub mod props;
 pub mod raw_expr;
 
 pub use component::*;
 pub use element::*;
-pub use location::*;
 pub use node::*;
 pub use props::*;
 pub use raw_expr::*;
